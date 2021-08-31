cf54395da224a07b91fe4a752849894a96acf2eb
gzocchi <z.giorgio.z@gmail.com>
2021-08-31 12:32:26 +0200

    add: types seed

---
+++ config/types.php
@@ -0,0 +1,52 @@
+<?php
+
+return $types = [
+    [
+        'name' => 'Americano',
+        'image' => 'americano.webp'
+    ],
+    [
+        'name' => 'Cinese',
+        'image' => 'cinese.webp'
+    ],
+    [
+        'name' => 'Giapponese',
+        'image' => 'giapponese.webp'
+    ],
+    [
+        'name' => 'Greco',
+        'image' => 'greco.webp'
+    ],
+    [
+        'name' => 'Indiano',
+        'image' => 'indiano.webp'
+    ],
+    [
+        'name' => 'Italiano',
+        'image' => 'italiano.webp'
+    ],
+    [
+        'name' => 'Libanese',
+        'image' => 'libanese.webp'
+    ],
+    [
+        'name' => 'Messicano',
+        'image' => 'messicano.webp'
+    ],
+    [
+        'name' => 'Pizza',
+        'image' => 'pizza.webp'
+    ],
+    [
+        'name' => 'Sushi',
+        'image' => 'sushi.webp'
+    ],
+    [
+        'name' => 'Thailandese',
+        'image' => 'thailandese.webp'
+    ],
+    [
+        'name' => 'Vegetariano',
+        'image' => 'vegetariano.webp'
+    ],
+];
--- database/seeds/TypesTableSeeder.php
+++ database/seeds/TypesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use App\Type;
 use Illuminate\Support\Str;
 
@@ -13,13 +14,20 @@ class TypesTableSeeder extends Seeder
      */
     public function run()
     {
-        $types = ['Cinese', 'Pizza', 'Italiano'];
+        $types = config('types');
+
+        $img_path = base_path().DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR.'types';
+        $dst_path = 'types';
 
         foreach ($types as $item) {
+            $contents = file_get_contents($img_path.DIRECTORY_SEPARATOR.$item['image']);
+            Storage::put($dst_path.DIRECTORY_SEPARATOR.$item['image'], $contents);
+
             $type = new Type();
 
-            $type->name = $item;
-            $type->slug = Str::slug($item, '-');
+            $type->name = $item['name'];
+            $type->slug = Str::slug($item['name'], '-');
+            $type->image = $dst_path.DIRECTORY_SEPARATOR.$item['image'];
 
             $type->save();
         }
