12d215f89230a972cba6e73fbbdd21a6afb076d3
gzocchi <z.giorgio.z@gmail.com>
2021-08-31 17:15:37 +0200

    mod: DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(TypesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(RestaurantsTableSeeder::class);
     }
 }
